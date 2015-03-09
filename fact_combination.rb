397e6bd53ed87369f769cd9149bfce0b56e293cc
Victor |Stalker| Skurikhin <stalker@quake.ru>
2015-03-09 23:51:40 +0300

    Changes to be committed: 	modified:   fact_combination.rb

--- fact_combination.rb
+++ fact_combination.rb
@@ -1,9 +1,15 @@
 #!/usr/bin/env ruby
-s1 = "0"
-s2 = "1"
-s3 = "2"
-s4 = "3"
-s5 = "4"
+s1 = "C∈A"
+s2 = "A⊇B"
+s3 = "D⊂A"
+s4 = "B∉A"
+s5 = "B∈A"
+s6 = "D∈B"
+s7 = "C⊆A"
+s8 = "A=B"
+s9 = "C∈B"
+s10 = "A⊆B"
+
 
 class Integer
   def fact
@@ -11,7 +17,7 @@ class Integer
   end
 end
 
-a = [s1, s2, s3, s4 ,s5 ];
+a = [s1, s2, s3, s4 ,s5, s6, s7, s8, s9, s10 ];
 n = a.size
 
 puts "n  = %i" % n
