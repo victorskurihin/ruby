77b3fa59a6b0c8a3a0f2ad2da1e591f2f983ca32
Victor |Stalker| Skurikhin <stalker@quake.ru>
2015-03-16 22:28:13 +0300

    Changes to be committed: 	new file:   stepic.org/Discrete_Math/Dainiak/1.4/Step_5/Step_5.txt 	new file:   stepic.org/Discrete_Math/Dainiak/1.4/Step_6/Step_6.rb 	new file:   stepic.org/Discrete_Math/Dainiak/1.4/Step_6/Step_6.txt 	new file:   stepic.org/Discrete_Math/Dainiak/1.4/Step_8/Step_8.rb 	new file:   stepic.org/Discrete_Math/Dainiak/1.4/Step_8/Step_8.txt

---
+++ stepic.org/Discrete_Math/Dainiak/1.4/Step_6/Step_6.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+# $Date$
+# $Id$
+# $Version: 0.1$
+# $Revision: 2$
+# $Author: Victor |Stalker| Skurikhin <stalker@quake.ru>$
+################################################################################
+(1..99).each do |n|
+  af = Float(n)
+  af = (af/2).floor
+  a = af
+  bf = Float(n)
+  bf = (bf/2).ceil
+  b = bf
+  y = a + b
+  printf("n = %2d, y(n) = a + b = %2d, n - 1 = %d\n", n, y, (n + 1))
+end
+
+
+__END__
+################################################################################
+# vim: syntax=ruby:fileencoding=utf-8:fileformat=unix:tw=78:ts=2:sw=2:sts=2:et
+# EOF
