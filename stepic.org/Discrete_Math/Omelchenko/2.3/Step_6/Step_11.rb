7ef8e2d029785f53efd2d84377b9b96e2f1b9a17
Victor |Stalker| Skurikhin <stalker@quake.ru>
2015-03-22 11:17:01 +0300

    Changes to be committed: 	new file:   stepic.org/Discrete_Math/Omelchenko/2.3/Step_6/Step_11.rb 	new file:   stepic.org/Discrete_Math/Omelchenko/2.3/Step_7/Step_7.rb

---
+++ stepic.org/Discrete_Math/Omelchenko/2.3/Step_6/Step_11.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+# $Date$
+# $Id$
+# $Version: 0.1$
+# $Revision: 2$
+# $Author: Victor |Stalker| Skurikhin <stalker@quake.ru>$
+################################################################################
+v = [3, 3]
+def v.apply(a)
+  a.each_index do |i|
+    c = ""
+    printf("v[%d] =", i);
+    a[i].each_index { |j|
+      # p a[i][j]*self[j]
+      printf("%s a[%d][%d] = %8p", c, i, j, a[j][i]);
+      c = " +"
+    }
+    puts
+  end
+end
+a = [[0, Rational(-1,3)], [Rational(1,3), 0]]
+a.each_index do |i|
+  a[i].each_index { |j|
+    printf("a[%d][%d] = %8p", i, j, a[i][j]);
+  }
+  puts
+end
+puts
+v.apply(a)
+__END__
+################################################################################
+# vim: syntax=ruby:fileencoding=utf-8:fileformat=unix:tw=78:ts=2:sw=2:sts=2:et
+# EOF
