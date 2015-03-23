9456ce53f5ffe17d16058c1982c9afb77855fe5c
Victor |Stalker| Skurikhin <stalker@quake.ru>
2015-03-23 12:00:21 +0300

    Changes to be committed: 	new file:   stepic.org/Discrete_Math/Omelchenko/3.2/Step_4/step4.rb 	new file:   stepic.org/Discrete_Math/Omelchenko/3.2/Step_6/step6.rb 	new file:   stepic.org/Discrete_Math/Omelchenko/3.2/Step_9/step9.rb 	new file:   stepic.org/Discrete_Math/Omelchenko/3.2/Step_9/step9.txt 	new file:   stepic.org/Discrete_Math/Omelchenko/3.3/Step_8/step8.rb 	new file:   stepic.org/Discrete_Math/Omelchenko/3.3/Step_9/step9.rb 	new file:   stepic.org/Discrete_Math/Omelchenko/3.3/Step_9/step9.txt

---
+++ stepic.org/Discrete_Math/Omelchenko/3.3/Step_8/step8.rb
@@ -0,0 +1,193 @@
+#!/usr/bin/env ruby
+# Пусть А = {1,2,3,4,5,6,7}, В = {4,5,6,7,8,9,10}, С = {2,4,6,8,10},
+# а U = {1,2,3,4,5,6,7,8,9,10}. Определите (A∪B)∩(U∖C).
+#
+# В качестве ответа перечислите элементы множества через пробел.
+# Например:
+# 1 2 3 4
+#
+class Numeric
+  def degrees
+    self * Math::PI / 180 
+  end
+  def undegrees
+    self / Math::PI * 180 
+  end
+end
+def distance(a, b)
+  r = Rational(0, 1)
+  a.each_index { |i|
+    t = Rational(a[i] - b[i])
+    r = r + t*t;
+  }
+  Math.sqrt(r)
+end
+class Array
+  def applyo(a)
+    v = []
+    a.each_index { |i|
+      c = ""
+      r  = 0
+      # printf(" v%d =", i)
+      a[i].each_index do |j|
+        # printf("%s a%d%d*%d |", c, i, j, self[j])
+        r = r + a[i][j]*self[j]
+        c = " +"
+      end
+      # puts
+      v[i] = r
+    }
+    v.each_index { |i|
+      self[i] = v[i]
+    }
+  end
+  def +(b)
+    if not b.kind_of? Array
+      return nil
+    end
+    if self.size != b.size
+      return nil
+    end
+    r = self.map{self[0].map{0}}
+    p r
+    self.each_index { |i|
+      if not self[i].kind_of? Array or not b[i].kind_of? Array
+        return nil
+      end
+      if self[i].size != b[i].size
+        return nil
+      end
+      #r = self[i].map{0}
+      self[i].each_index do |j|
+        r[i][j] = self[i][j] + b[i][j]
+      end
+    }
+    r
+  end
+  def *(x)
+    if not x.kind_of? Array
+      return nil
+    end
+    a = nil
+    b = nil
+    r = [[]]
+    min, max = 0, 0
+    if self.size <= x.size
+      min, max = self.size, x.size
+      a, b = self, x
+    else
+      min, max = x.size, self.size
+      a, b = x, self
+    end
+    i = 0
+    while i < min
+      j = 0
+      v = b[i].map{0}
+      while j < min
+        k = 0
+        c = ""
+        while k < max
+          v[j] = v[j] + a[i][k]*b[k][j]
+          #printf("%s v%d = v%d + a%d%d*b%d%d",
+          #       c, j, j, i, k, k, j)
+          c = " +"
+          k += 1
+        end
+        #puts
+        j += 1
+      end
+      r[i] = v
+      i += 1
+    end
+    r
+  end
+  def scalar_product(v)
+    r = Rational(0, 1)
+    v.each_index { |i|
+      r = r + self[i]*v[i]
+    }
+    r
+  end
+  def modulus_vector
+    r = Rational(0, 1)
+    self.each_index { |i|
+      r = r + self[i]*self[i]
+    }
+    Math.sqrt(r)
+  end
+  def proj!(v)
+    r1 = scalar_product(v)
+    r2 = scalar_product(self)
+    self.each_index { |i|
+      self[i] = self[i]*r1/r2
+    }
+  end
+  def proj(v)
+    r = []
+    r1 = scalar_product(v)
+    r2 = scalar_product(self)
+    self.each_index { |i|
+      r[i] = self[i]*r1/r2
+    }
+    r
+  end
+end
+def ∪(a, b)
+  if not a.kind_of? Array
+    return nil
+  end
+  if not b.kind_of? Array
+    return nil
+  end
+  r = Hash.new
+  a.each { |e|
+    r[e] = 1
+  }
+  b.each { |e|
+    r[e] = 1
+  }
+  r.keys
+end
+def ∩(a,b)
+  if not a.kind_of? Array
+    return nil
+  end
+  if not b.kind_of? Array
+    return nil
+  end
+  r = Hash.new
+  a.each { |e|
+    if b.member?(e)
+      r[e] = 1
+    end
+  }
+  r.keys
+end
+def ∖(a,b)
+  if not a.kind_of? Array
+    return nil
+  end
+  if not b.kind_of? Array
+    return nil
+  end
+  r = Hash.new
+  a.each { |e|
+    if not b.member?(e)
+      r[e] = 1
+    end
+  }
+  r.keys
+end
+i = 0
+c = 0
+(1..20).each {|e1|
+  (1..20).each {|e2|
+    i += 1
+    if 21 == (e1 + e2)
+      c += 1
+      printf("%d + %d == 21 | i = %d | count =%d\n", e1, e2, i, c)
+    end
+  }
+}
+printf("i = %d\n", i)
+__END__
