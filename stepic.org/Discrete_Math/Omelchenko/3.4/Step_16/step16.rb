ef19eb4f279302f729fbffc010ecaf8316758800
Victor (Stalker) Skurikhin <stalker@quake.ru>
2015-03-28 09:55:27 +0300

    Changes to be committed: 	modified:   stepic.org/Discrete_Math/Omelchenko/3.4/Step_16/step16.rb

--- stepic.org/Discrete_Math/Omelchenko/3.4/Step_16/step16.rb
+++ stepic.org/Discrete_Math/Omelchenko/3.4/Step_16/step16.rb
@@ -14,15 +14,18 @@ n = 42
 f = sprintf("%%0%db", n)
 count = 0
 b = 0
-e = 2**n-1
+e = 2**n - 1
 r = Range.new(b, e)
 p = ""
-r.each do |n|
-  str_bit_num = sprintf(f, n)
+r.each do |i|
+  str_bit_num = sprintf(f, i)
+  if str_bit_num.size > n
+    break
+  end
   if 7 == str_bit_num.count("1") 
     if not str_bit_num =~ /(11)/
-      # printf(f + "\n", n)
       count += 1
+      printf(f + " count : %d " + "\n", n, count)
     end
   end
 end
