104791c75fe802ce1c081166eb0b4770576237fb
Victor |Stalker| Skurikhin <stalker@quake.ru>
2015-04-26 23:02:30 +0300

    Changes to be committed: 	new file:   stepic.org/Discrete_Math/Omelchenko/7.3/Step_7/Step_7.rb 	new file:   stepic.org/Discrete_Math/Omelchenko/7.4/Step_4/Step_4.rb 	new file:   stepic.org/Discrete_Math/Omelchenko/7.4/Step_5/Step_5.rb 	new file:   stepic.org/Discrete_Math/Omelchenko/7.4/Step_7/Step_7.rb 	new file:   stepic.org/Discrete_Math/Omelchenko/7.4/Step_9/Step_9.rb 	modified:   stepic.org/Discrete_Math/lib.rb 	new file:   stepic.org/MatAn/3.5/Step_6/step_6.rb

--- stepic.org/Discrete_Math/Omelchenko/7.3/Step_7/Step_7.rb
+++ stepic.org/Discrete_Math/Omelchenko/7.3/Step_7/Step_7.rb
@@ -4,9 +4,16 @@
 # $Date$
 # $Id$
 # $Version: 0.1$
-# $Revision: 6$
+# $Revision: 5$
 # $Author: Victor |Stalker| Skurikhin <stalker@quake.ru>$
 ################################################################################
+# На гранях кубика вместо меток 1, 2, 3, 4, 5 и 6 нанесены метки
+# 1, 3, 4, 5, 6, 8. Какие метки должны быть на гранях второго кубика, чтобы
+# распределение суммы очков при бросании двух этих кубиков было таким же, как и
+# для случая бросания двух обычных кубиков?
+#
+# В качестве ответа перечислите метки через запятую без пробелов в порядке
+# неубывания. Например: 1,2,3,4,5,6
 
 class Integer
   def fact
@@ -20,12 +27,6 @@ class Float
     Math.sqrt(2*Math::PI*self)*(self/Math::E)**self
   end
   alias factorial fact
-  def to_rad
-    self*Math::PI/180
-  end
-  def to_gradus
-    (self*180).to_f/Math::PI
-  end
 end
 # extremum 170.6.fact
 
@@ -109,6 +110,30 @@ def luc(n)
     return r
   end
 end
+
+def step4(a, b)
+  c = 0
+  r = Hash.new
+  a.combination(1) { |ac|
+    b.combination(1) do |bc|
+      c += 1
+      s = ac[0] + bc[0]
+      if r[s] then
+        r[s]+=1
+      else
+        r[s]=1
+      end
+    end
+  }
+  return c, r
+end
+
+a=[1,2,3,4,5,6]
+b=[1,2,3,4,5,6]
+p step4(a, b)
+a=[1,3,4,5,6,8]
+b=[1,2,2,3,3,4]
+p step4(a, b)
 
 __END__
 ################################################################################
