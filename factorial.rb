f9cf979ba250aea47256cbc91d7a1a889da46e22
Victor |Stalker| Skurikhin <stalker@quake.ru>
2015-06-26 16:16:33 +0300

    Changes to be committed: 	new file:   coursera.org/Algorithmic_Thinking/Module_2_-_Algorithmic_efficiency_and_BFS.py 	new file:   coursera.org/Algorithmic_Thinking/Module_2_-_Algorithmic_efficiency_and_BFS_v0.1.py 	new file:   coursera.org/Algorithmic_Thinking/Module_2_-_Algorithmic_efficiency_and_BFS_v0.2.py 	new file:   coursera.org/Algorithmic_Thinking/Module_2_-_Algorithmic_efficiency_and_BFS_v0.3.py 	new file:   coursera.org/Algorithmic_Thinking/Module_2_-_Algorithmic_efficiency_and_BFS_v0.4.py 	new file:   coursera.org/Algorithmic_Thinking/Module_2_-_Algorithmic_efficiency_and_BFS_v0.5.py 	new file:   coursera.org/Algorithmic_Thinking/Module_2_-_Algorithmic_efficiency_and_BFS_v0.6.py 	new file:   coursera.org/Algorithmic_Thinking/Module_2_-_Algorithmic_efficiency_and_BFS_v0.7.py 	new file:   coursera.org/Algorithmic_Thinking/Module_2_-_Algorithmic_efficiency_and_BFS_v0.8.py 	new file:   coursera.org/Algorithmic_Thinking/Module_2_Algorithmic_efficiency_and_BFS_tst.py 	modified:   factorial.rb

--- factorial.rb
+++ factorial.rb
@@ -1,4 +1,19 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+################################################################################
+# $Date$
+# $Id$
+# $Version: 2.1$
+# $Revision: 3$
+# $Author: Victor |Stalker| Skurikhin <stalker@quake.ru>$
+################################################################################
+#
+
 require 'numtheory' # gem install ruby-numtheory
+require 'bigdecimal'
+require 'bigdecimal/math'
+
+include BigMath
 
 def product(arr)
     def rec_product(n, step, arr)
@@ -11,9 +26,10 @@ def product(arr)
     rec_product(arr.length-1, 1, arr)
 end
 
-$small_odd_swing = [ 1,1,1,3,3,15,5,35,35,315,63,693,231,3003,429,6435,6435,109395,
-                  12155,230945,46189,969969,88179,2028117,676039,16900975,1300075,
-                  35102025,5014575,145422675,9694845,300540195,300540195 ]
+$small_odd_swing = [ 1,1,1,3,3,15,5,35,35,315,63,693,231,3003,429,6435,6435,
+                     109395,12155,230945,46189,969969,88179,2028117,676039,
+                     16900975,1300075, 35102025,5014575,145422675,9694845,
+                     300540195,300540195 ]
 
 PRECOMPUTED_ODD_SWINGS_NUM = $small_odd_swing.length
 
@@ -69,4 +85,49 @@ def factorial_primeswing(n)
     return rec_factorial(n) << shift
 end
 
-puts factorial_primeswing(100000)
+class Integer
+  def slow_fact
+    (1..self).reduce(:*) || 1
+  end
+  # limit for Debian 8 Linux 3.16.0-4-amd64 #1 SMP 3.16.7-2 (2014-11-06) x86_64
+  # is 939002
+  def unstable_fact
+    factorial_primeswing(self)
+  end
+  alias fact unstable_fact
+  alias factorial slow_fact
+end
+
+class Float
+  # extremum 170.6.fact
+  def fact
+    Math.sqrt(2*Math::PI*self)*(self/Math::E)**self
+  end
+  alias factorial fact
+  def to_rad
+    self*Math::PI/180
+  end
+  def to_gradus
+    (self*180).to_f/Math::PI
+  end
+end
+
+__END__
+
+for n in (160..170)
+  printf("(%d).to_f.fact = %e\n", n, n.to_f.fact)
+  r = factorial_primeswing(n)
+  printf("factorial_primeswing(%d) = 0.%s*10^%d\n", n, r.to_s[0..9], r.to_s.length)
+end
+n = 939002
+r = factorial_primeswing(n)
+puts (BigMath.log(r,1)/BigMath.log(10,1)).to_i
+printf("factorial_primeswing(%d) = 0.%s*10^%d\n", n, r.to_s[0..9], r.to_s.length)
+for n in (939000..939999)
+  r = factorial_primeswing(n)
+  printf("factorial_primeswing(%d) = 0.%s*10^%d\n", n, r.to_s[0..9], r.to_s.length)
+end
+
+################################################################################
+# vim: syntax=ruby:fileencoding=utf-8:fileformat=unix:tw=78:ts=2:sw=2:sts=2:et
+# EOF
