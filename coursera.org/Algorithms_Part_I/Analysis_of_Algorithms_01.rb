b8c36d8ae689baae41f4c61efab723eeb96b4534
Victor |Stalker| Skurikhin <stalker@quake.ru>
2015-07-13 16:31:55 +0300

    modified:   coursera.org/Algorithms_Part_I/Analysis_of_Algorithms_01.rb modified:   coursera.org/Algorithms_Part_I/Analysis_of_Algorithms_02.rb

--- coursera.org/Algorithms_Part_I/Analysis_of_Algorithms_01.rb
+++ coursera.org/Algorithms_Part_I/Analysis_of_Algorithms_01.rb
@@ -7,32 +7,31 @@
 # $Revision: 2$
 # $Author: Victor |Stalker| Skurikhin <stalker@quake.ru>$
 ################################################################################
-(seed = 957781)
-Suppose that you time a program as a function of N and produce
-the following table.
-
-        N   seconds
--------------------
-      512     0.000
-     1024     0.001
-     2048     0.003
-     4096     0.015
-     8192     0.069
-    16384     0.313
-    32768     1.436
-    65536     6.440
-   131072    29.199
-   262144   133.035
-   524288   602.118
-  1048576  2739.285
-
-
-Estimate the order of growth of the running time as a function of N.
-Assume that the running time obeys a power law T(N) ~ a N^b. For your
-answer, enter the constant b. Your answer will be marked as correct
-if it is within 1% of the target answer - we recommend using
-two digits after the decimal separator, e.g., 2.34.
-
+# (seed = 957781)
+# Suppose that you time a program as a function of N and produce
+# the following table.
+# 
+#         N   seconds
+# -------------------
+#       512     0.000
+#      1024     0.001
+#      2048     0.003
+#      4096     0.015
+#      8192     0.069
+#     16384     0.313
+#     32768     1.436
+#     65536     6.440
+#    131072    29.199
+#    262144   133.035
+#    524288   602.118
+#   1048576  2739.285
+# 
+# 
+# Estimate the order of growth of the running time as a function of N.
+# Assume that the running time obeys a power law T(N) ~ a N^b. For your
+# answer, enter the constant b. Your answer will be marked as correct
+# if it is within 1% of the target answer - we recommend using
+# two digits after the decimal separator, e.g., 2.34.
 
 def f(b)
     a = 0.001/(256**b)
@@ -76,62 +75,119 @@ f(b)
 #                                                          4 (padding to round up to a multiple of 8)
 #}                                                      ----
 #                                                        264
-The correct answer is: 288
+# The correct answer is: 288
+# 
+# public class MysteryBox {                           //   16 (object overhead)
+#     private final int x0;                           //    4 (1 int)
+#     private final double y0, y1, y2, y3;            //   32 (4 double)
+#     private final long z0, z1, z2, z3;              //   32 (4 long)
+#     private final boolean[] a = new boolean[168];   //    8 (reference to array)
+#                                                     //  192 (boolean array of size 168)
+#     ...                                                   4 (padding to round up to a multiple of 8)
+# }                                                      ----
+#                                                         288
+
+public class MysteryBox {                             //   16 (object overhead)
+    private final double x0, x1;                      //   16 (2 double)
+    private final int y0, y1;                         //    8 (1 int)
+    private final boolean z0, z1, z2;                 //    3 (3 boolean)
+    private final long[] a = new long[88];            //  728 (long array of size 88)
+
+    ...
+}
+                                                          776
+The correct answer is: 784
 
 public class MysteryBox {                           //   16 (object overhead)
-    private final int x0;                           //    4 (1 int)
-    private final double y0, y1, y2, y3;            //   32 (4 double)
-    private final long z0, z1, z2, z3;              //   32 (4 long)
-    private final boolean[] a = new boolean[168];   //    8 (reference to array)
-                                                    //  192 (boolean array of size 168)
-    ...                                                   4 (padding to round up to a multiple of 8)
+    private final double x0, x1;                    //   16 (2 double)
+    private final int y0, y1;                       //    8 (2 int)
+    private final boolean z0, z1, z2;               //    3 (3 boolean)
+    private final long[] a = new long[88];          //    8 (reference to array)
+                                                    //  728 (long array of size 88)
+    ...                                                   5 (padding to round up to a multiple of 8)
 }                                                      ----
-                                                        288
-
-
-
-(seed = 395624)
-Suppose that you time a program as a function of N and produce
-the following table.
-
-        N   seconds
--------------------
-      256     0.000
-      512     0.002
-     1024     0.015
-     2048     0.091
-     4096     0.557
-     8192     3.405
-    16384    20.789
-    32768   128.815
-    65536   786.702
-   131072  4856.110
-
-
-Estimate the order of growth of the running time as a function of N.
-Assume that the running time obeys a power law T(N) ~ a N^b. For your
-answer, enter the constant b. Your answer will be marked as correct
-if it is within 1% of the target answer - we recommend using
-two digits after the decimal separator, e.g., 2.34.
+                                                        784
 
 
 
 
-The theoretical order-of-growth is N ^ (34/13) = 2.62
+public class MysteryBox {A                          //   16 (object overhead)
+    private final int x0, x1;                       //    8 (2 int)
+    private final long y0, y1, y2;                  //   24 (3 long)
+    private final double z0, z1, z2;                //   24 (3 long)
+    private final boolean[] a = new boolean[216];   //    8 (reference to array)
+                                                    //  240  (boolean array of size 216)
+    ...                                                 ---
+}                                                       240
 
-The empirical order-of-growth is N ^ (log_2 ratio)
 
-                                  log_2
-        N   seconds     ratio     ratio
----------------------------------------
-      256     0.000         -         -
-      512     0.002         -         -
-     1024     0.015      7.50      2.91
-     2048     0.091      6.07      2.60
-     4096     0.557      6.12      2.61
-     8192     3.405      6.11      2.61
-    16384    20.789      6.11      2.61
-    32768   128.815      6.20      2.63
-    65536   786.702      6.11      2.61
-   131072  4856.110      6.17      2.63
+The correct answer is: 320
 
+public class MysteryBox {                           //   16 (object overhead)
+    private final int x0, x1;                       //    8 (2 int)
+    private final long y0, y1, y2;                  //   24 (3 long)
+    private final double z0, z1, z2;                //   24 (3 double)
+    private final boolean[] a = new boolean[216];   //    8 (reference to array)
+                                                    //  240 (boolean array of size 216)
+    ...                                                   0 (padding to round up to a multiple of 8)
+}                                                      ----
+                                                        320
+
+
+
+public class MysteryBox {                           //   16 (object overhead)
+    private final boolean x0, x1, x2;               //    3 (3 boolean)
+    private final int y0, y1, y2;                   //   12 (3 int)
+    private final long z0, z1, z2, z3;              //   32 (4 long)
+    private final double[] a = new double[80];      //    8 (reference to array)
+                                                    //  664 (double array of size 80)
+    ...                                                 
+}                                                       ---
+                                                        736
+
+
+
+
+public class MysteryBox {                           //   16 (object overhead)
+    private final double x0, x1;                    //   16 (2 double)
+    private final boolean y0, y1;                   //    2 (2 boolean)
+    private final long z0, z1;                      //   16 (2 long)
+    private final int[] a = new int[32];            //    8 (reference to array)
+                                                    //  152 (int array of size 32)
+    ...                                                 210 + 6 
+}                                                       ---
+                                                        216
+
+
+
+(seed = 403083)
+What is the order of growth of the worst case running time of the following code fragment
+as a function of N?
+
+int sum = 0;
+for (int i = 0; i*i < N; i++)
+    for (int j = 0; j*j < 4*N; j++)
+        for (int k = 0; k < N*N; k++)
+            sum++;
+
+
+The answer is : N^3
+
+The i loop iterates N^(1/2) times; the j loop iterates 2 N^(1/2) times; the k loop iterates N^2 times.
+
+
+
+
+
+
+
+
+
+int sum = 0;
+for (int i = 0; i*i*i < N; i++)
+    for (int j = 0; j*j*j < N; j++)
+        for (int k = 0; k*k*k < N; k++)
+            sum++;A
+
+
+The i loop iterates N^(1/3) times; the j loop iterates N^(1/3) times; the k loop iterates N^(1/3) times.
